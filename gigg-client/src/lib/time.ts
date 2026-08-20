7d9858c7843e34f1131fb9efcbd563f186a64397
Prabha-Jeyaraj <prabhaprasanna848@gmail.com>
2026-08-20 15:18:49 +0530

    fix: format dashboard My Jobs and Available Jobs reporting time in 12-hour clock and export time formatters

--- gigg-client/src/lib/time.ts
+++ gigg-client/src/lib/time.ts
@@ -1,1 +1,5 @@
-export { formatTime12Hour, formatTime12Hour as formatTimeString12h } from '../utils/formatters';
+export {
+  formatTime12Hour,
+  formatTime12Hour as formatTime12h,
+  formatTime12Hour as formatTimeString12h,
+} from '../utils/formatters';
